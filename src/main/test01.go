34994d02398c8a6a3833972f03985ed581c0cdc5
lizhixin <lzx920911@gmail.com>
2019-04-02 15:39:51 +0800

    set logrus time as we want

--- src/main/test01.go
+++ src/main/test01.go
@@ -6,8 +6,14 @@ import (
 )
 
 func init() {
-	// 设置日志格式为json格式　自带的只有两种样式logrus.JSONFormatter{}和logrus.TextFormatter{}
-	log.SetFormatter(&log.JSONFormatter{})
+	// 设置日志格式为json格式, 此处时间格式是当前时间　自带的只有两种样式logrus.JSONFormatter{}和logrus.TextFormatter{}
+	//log.SetFormatter(&log.JSONFormatter{})
+
+	// 设置日志格式为TextFormatter,设置输出的时间为当前时间（不设置的话就是服务启动的时间，比如0000）
+	//log.SetFormatter(&log.TextFormatter{
+	//	TimestampFormat: "2006-01-02T15:04:05.000",
+	//	FullTimestamp: true,
+	//})
 
 	// 设置将日志输出到标准输出（默认的输出为stderr，标准错误）
 	// 日志消息输出可以是任意的io.writer类型

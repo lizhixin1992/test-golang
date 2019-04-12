741848d87048ffdad354cf20592e6bf0d6fa5417
lizhixin <lzx920911@gmail.com>
2019-04-12 12:00:38 +0800

    测试iris 分组路由

--- src/main/IrisTest.go
+++ src/main/IrisTest.go
@@ -12,17 +12,50 @@ func main() {
 	app.Use(recover.New())
 	app.Use(logger.New())
 
-	app.Handle("GET", "/", func(ctx iris.Context) {
-		ctx.HTML("<h1>Welcome</h1>")
-	})
+	app.Handle("GET", "/", index)
 
-	app.Get("/ping", func(ctx iris.Context) {
-		ctx.WriteString("pong")
-	})
+	party := app.Party("/party", partyTest)
 
-	app.Get("/hello", func(ctx iris.Context) {
-		ctx.JSON(iris.Map{"message": "Hello Iris!"})
-	})
+	party.Get("/ping", ping)
+
+	party.Get("/hello", hello)
+
+	users := app.Party("/users", myAuthMiddlewareHandler)
+	// http://localhost:8080/users/42/profile
+	users.Get("/{id:int}/profile", userProfileHandler)
+	// http://localhost:8080/users/inbox/1
+	users.Get("/inbox/{id:int}", userMessageHandler)
 
 	app.Run(iris.Addr(":8080"), iris.WithConfiguration(iris.YAML("./src/main/iris.yml")))
+}
+
+func index(ctx iris.Context) {
+	ctx.HTML("<h1>Welcome</h1>")
+}
+
+func partyTest(ctx iris.Context) {
+	ctx.WriteString("Authentication failed")
+	ctx.Next()
+}
+
+func ping(ctx iris.Context) {
+	ctx.WriteString("111")
+}
+
+func hello(ctx iris.Context) {
+	ctx.JSON(iris.Map{"message": "Hello Iris!"})
+}
+
+func myAuthMiddlewareHandler(ctx iris.Context) {
+	ctx.WriteString("Authentication failed")
+	//允许运行后续的逻辑
+	ctx.Next()
+}
+func userProfileHandler(ctx iris.Context) { //
+	id := ctx.Params().Get("id")
+	ctx.WriteString(id)
+}
+func userMessageHandler(ctx iris.Context) {
+	id := ctx.Params().Get("id")
+	ctx.WriteString(id)
 }
